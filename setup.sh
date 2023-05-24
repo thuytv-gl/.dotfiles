aab391a6d70d423d2c46e2cf3887c436fa6b02df
thuy tran van <thuytranvan.gl@gmail.com>
2023-05-25 01:01:46 +0700

    chore(): update i3 config, set pretty background

--- setup.sh
+++ setup.sh
@@ -9,4 +9,5 @@ ln -s $DOT_ROOT/zsh/* $HOME/
 ln -s $DOT_ROOT/i3/.i3status.conf $HOME/
 ln -s $DOT_ROOT/i3/config $HOME/.config/i3/
 ln -s $DOT_ROOT/nvim $HOME/.config
+ln -s $DOT_ROOT/ranger $HOME/.config
 
