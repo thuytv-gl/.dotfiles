41cc949e1fd66a7788191b4ad41a1f9d52cac7e7
thuy tran van <thuytranvan.gl@gmail.com>
2023-06-06 21:43:18 +0700

    add pjrun.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ set +x
 DOT_ROOT=$HOME/.dotfiles
 
 ln -s $DOT_ROOT/tmux/.tmux.conf $HOME/
+ln -s $DOT_ROOT/tmux/pjrun.sh $HOME/pjrun.sh
 ln -s $DOT_ROOT/alacritty.yml $HOME/.alacritty.yml
 ln -s $DOT_ROOT/zsh/* $HOME/
 ln -s $DOT_ROOT/i3/.i3status.conf $HOME/
