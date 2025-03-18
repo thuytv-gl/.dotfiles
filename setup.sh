f3186b39a196e5a8153267668007ceb2e4f8e25c
Thuy Tran Van <thuytravnan.gl@gmail.com>
2025-03-18 10:14:26 +0700

    move nvim to a seperate repository

--- setup.sh
+++ setup.sh
@@ -9,6 +9,5 @@ ln -s $DOT_ROOT/alacritty.yml $HOME/.alacritty.yml
 ln -s $DOT_ROOT/zsh/* $HOME/
 ln -s $DOT_ROOT/i3/.i3status.conf $HOME/
 ln -s $DOT_ROOT/i3/config $HOME/.config/i3/
-ln -s $DOT_ROOT/nvim $HOME/.config
 ln -s $DOT_ROOT/ranger $HOME/.config
 
