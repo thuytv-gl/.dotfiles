843271f17b6d72b0f9d1e22624f3c2ffad64ff56
thuy tran van <thuytranvan.gl@gmail.com>
2025-10-05 21:35:27 +0700

    add some spice

--- i3/scripts/wallpaper.sh
+++ i3/scripts/wallpaper.sh
@@ -1,52 +1,9 @@
 #!/bin/bash
-set -o nounset
-pushd ~/.dotfiles/wallpapers > /dev/null
 
-[ ! -f .current ] && touch .current
-
-set_next=0
-first=$(ls | head -1)
-last=$(ls | tail -1)
-
-set_wallpaper() {
-    wp_file=$(realpath "${1:-}")
-    feh --bg-scale "${wp_file}"
-    echo ${wp_file} > .current
-}
-
-get_next_wallpaper() {
-  local current_wp=$(basename $1)
-  if [ "$current_wp" == "$last" ] ; then
-    echo $first
-    exit 0
-  fi 
-  ls | while read file; do
-    if [ $set_next == 1 ];then
-      realpath $file
-      exit 0
-    fi
-
-    if [ "$current_wp" == "$file" ]; then
-      set_next=1
-    fi
-  done
-}
-
-get_current_wallpaper() {
-  local current=$(cat .current)
-  if [ -z "$current" ] || [ ! -f "$current" ]; then
-    echo $(realpath $first)
-    exit 0
-  fi
-  echo $(realpath $current)
-}
-
-current_wp=$(get_current_wallpaper)
-opt=${1:-""}
+WALL_DIR=~/.dotfiles/wallpapers/poseidon
 
 if [ "$opt" == "--load" ]; then
-  set_wallpaper "$current_wp"
+  source ~/.fehbg
 else
-  set_wallpaper `get_next_wallpaper $current_wp`
+  exec feh -r --randomize --bg-scale $WALL_DIR 
 fi
-popd > /dev/null
---
+++ i3blocks/scripts/volume.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+_button="${button}"
+
+unmute() {
+  if pactl get-sink-mute @DEFAULT_SINK@ | grep -q yes; then
+      pactl set-sink-mute @DEFAULT_SINK@ toggle
+  fi
+}
+
+case $_button in
+  "1")
+    pactl set-sink-mute @DEFAULT_SINK@ toggle
+    ;;
+
+  "4")
+    unmute
+    pactl set-sink-volume @DEFAULT_SINK@ +5%
+    ;;
+
+  "5")
+    unmute
+    pactl set-sink-volume @DEFAULT_SINK@ -5%
+    ;;
+esac
+
+if pactl get-sink-mute @DEFAULT_SINK@ | grep -q yes; then
+  echo "♫ 0%"
+else
+  pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '\d+(?=%)' | head -1 | awk '{print "♫ "$1"%"}';
+fi
